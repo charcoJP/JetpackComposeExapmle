cdfc828c0b2dcce6a65f2b9b619bb4616a980b42
charcoJP <charco2356@gmail.com>
2019-11-30 00:00:48 +0900

    デフォルトの色に戻す

--- app/src/main/java/jp/co/charco/composeexample/CustomTheme.kt
+++ app/src/main/java/jp/co/charco/composeexample/CustomTheme.kt
@@ -7,20 +7,20 @@ import androidx.ui.material.MaterialColors
 import androidx.ui.material.MaterialTheme
 import androidx.ui.text.TextStyle
 
-val green = Color(0xFF1EB980.toInt())
-val grey = Color(0xFF26282F.toInt())
-private val themeColors = MaterialColors(
-    primary = green,
-    surface = grey,
-    onSurface = Color.White
-)
+//val green = Color(0xFF1EB980.toInt())
+//val grey = Color(0xFF26282F.toInt())
+//private val themeColors = MaterialColors(
+//    primary = green,
+//    surface = grey,
+//    onSurface = Color.White
+//)
 
 @Composable
 fun CustomTheme(children: @Composable() () -> Unit) {
-    MaterialTheme(colors = themeColors) {
-        val textStyle = TextStyle(color = Color.Red)
-        CurrentTextStyleProvider(value = textStyle) {
-            children()
-        }
+    MaterialTheme(/**colors = themeColors**/) {
+//        val textStyle = TextStyle(color = Color.Red)
+//        CurrentTextStyleProvider(value = textStyle) {
+//            children()
+//        }
     }
 }
