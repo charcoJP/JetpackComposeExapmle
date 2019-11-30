cff9cfdd87708980a06225effd2e00856546cd44
charcoJP <charco2356@gmail.com>
2019-11-30 12:37:22 +0900

    戻るボタンを追加

--- app/src/main/java/jp/co/charco/composeexample/list/ListExampleScreen.kt
+++ app/src/main/java/jp/co/charco/composeexample/list/ListExampleScreen.kt
@@ -1,18 +1,25 @@
 package jp.co.charco.composeexample.list
 
 import androidx.compose.Composable
+import androidx.compose.ambient
 import androidx.compose.unaryPlus
+import androidx.ui.core.ContextAmbient
 import androidx.ui.core.Text
 import androidx.ui.core.dp
 import androidx.ui.foundation.VerticalScroller
+import androidx.ui.graphics.imageFromResource
 import androidx.ui.layout.Column
 import androidx.ui.layout.FlexColumn
 import androidx.ui.layout.Padding
+import androidx.ui.material.AppBarIcon
 import androidx.ui.material.ListItem
 import androidx.ui.material.TopAppBar
 import androidx.ui.material.surface.Card
 import androidx.ui.material.themeTextStyle
 import androidx.ui.tooling.preview.Preview
+import jp.co.charco.composeexample.NavigationManager
+import jp.co.charco.composeexample.R
+import jp.co.charco.composeexample.Screen
 
 @Composable
 fun ListExampleScreen() {
@@ -21,6 +28,8 @@ fun ListExampleScreen() {
 
 @Composable
 private fun Content() {
+    val context = +ambient(ContextAmbient)
+
     // LinearLayout (orientation: Vertical)
     FlexColumn {
         // height: wrap_content
@@ -31,6 +40,13 @@ private fun Content() {
                         text = "リスト",
                         style = +themeTextStyle { subtitle1 }
                     )
+                },
+                navigationIcon = {
+                    AppBarIcon(
+                        imageFromResource(context.resources, R.drawable.arrow_back)
+                    ) {
+                        NavigationManager.navigateTo(Screen.Home)
+                    }
                 }
             )
         }
